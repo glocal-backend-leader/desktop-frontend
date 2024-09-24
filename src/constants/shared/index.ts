45c764f417116eb0cc1190c1a02845310f55cda6
glocal-dev1 <glocal_dev1@umanizales.edu.co>
2024-09-24 17:31:43 -0500

    FE2 (feat): sidebar listProcessType + config

--- src/constants/shared/index.ts
+++ src/constants/shared/index.ts
@@ -1,107 +1,32 @@
 export const BREADCRUMB = {
-    'academic-management': 'Gestión académica',
-    'academic-offerings': 'Oferta académica',
-    'academic-program': 'Programas académicos',
-    'administrative-management': 'Gestión administrativa',
-    'applicant': 'Aspirantes',
-    'assignment': 'Asignaciones',
-    'badge': 'Insignias',
-    'benefit': 'Beneficios',
-    'document': 'Documentos',
-    'document-issuance': 'Emisión de documentos',
-    'document-management': 'Gestión documental',
-    'digital-signature': 'Firmas digitales',
-    'elective': 'Electivas',
-    'flow': 'Flujos',
-    'flow-category': 'Categorías',
-    'flow-type': 'Tipos de flujo',
-    'internationalization-management': 'Gestión de la internacionalización',
-    'login': 'Entrar',
-    'program-special-enrollment': 'Programas para inscripción especial',
-    'request': 'Solicitudes',
-    'saia': 'SAIA',
-    'special-admission': 'Admisiones especiales',
-    'special-enrollment': 'Inscripciones especiales',
-    'status': 'Estados',
-    'student': 'Estudiantes',
-    'template': 'Plantillas',
-    'template-type': 'Tipos de plantilla',
-    'trigger': 'Disparadores',
-    'workflow': 'Flujo de trabajo'
+    'agreement':'Convenios',
+    'convocation':'Convocatorias',
+    'project':'Projectos',
+    'inbox':'Bandeja de entrada',
 };
 
+
 export const MENU = [
-    {
-        name: 'Administrador',
-        options: [
-            {
-                icon: 'academic_management/applicant/special_admission',
-                name: 'Procesos',
-                path: '/process'
-            },
-            {
-                icon: 'academic_management/applicant/special_admission',
-                name: 'Fases',
-                path: '/phase'
-            },
-            {
-                icon: 'academic_management/applicant/special_admission',
-                name: 'Formularios',
-                path: '/form'
-            },
-        ]
-    },
-    {
-        name: 'Movilidad',
-        options: [
-            {
-                icon: 'academic_management/applicant/special_admission',
-                name: 'Procesos',
-                path: '/academic-management/applicant/special-admission'
-            },
-        ]
-    },
-    {
-        name: 'Convenios',
-        options: [
-            {
-                icon: 'academic_management/applicant/special_admission',
-                name: 'Procesos',
-                path: '/academic-management/applicant/special-admission'
-            },
-        ]
-    },
-    {
-        name: 'Proyectos',
-        options: [
-            {
-                icon: 'academic_management/applicant/special_admission',
-                name: 'Procesos',
-                path: '/academic-management/applicant/special-admission'
-            },
-        ]
-    },
-    {
-        name: 'Insignias',
-        options: [
-            {
-                icon: 'academic_management/applicant/special_admission',
-                name: 'Procesos',
-                path: '/academic-management/applicant/special-admission'
-            },
-        ]
-    },
-    {
-        name: 'Reportes',
-        options: [
-            {
-                icon: 'academic_management/applicant/special_admission',
-                name: 'Procesos',
-                path: '/academic-management/applicant/special-admission'
-            },
-        ]
-    }
-]
+{
+    name: 'Configuración',
+    options: [
+        {
+            icon: 'shared/setting',
+            name: 'Convocatorias',
+            path: '/configuration/convocation'
+        },
+        {
+            icon: 'shared/setting',
+            name: 'Convenios',
+            path: '/configuration/agreement'
+        },
+        {
+            icon: 'shared/setting',
+            name: 'Proyectos',
+            path: '/configuration/project'
+        },
+    ]
+}]
 
 export const PUBLIC_ROUTES = [
     '/'

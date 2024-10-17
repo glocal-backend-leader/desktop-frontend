20e1cd4639cedf06a40f452575e2fb7e6439b27d
glocal-dev1 <glocal_dev1@umanizales.edu.co>
2024-10-17 16:11:45 -0500

    feat(token, login, sidebar): add token with astro.cookies and organized of login with redirected to homepage and create the dinamic sidebar with the menu associated with the role

--- src/constants/shared/index.ts
+++ src/constants/shared/index.ts
@@ -4,34 +4,10 @@ export const BREADCRUMB = {
     'project': 'Projectos',
     'inbox': 'Bandeja de entrada',
 };
-const data = {
-    email: "juan@gmail.com",
-    password: "juan21",
-    tenant_id: "99bb1d99-60bf-464d-b8ea-3dfb01d777ca" };
-
-const prueba = await fetch('http://172.28.20.251:3000/api/v1/user/login', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify(data)
-})
-  .then(async response => await response.json())
-  .then(async data => await data)
-  .catch(error => console.error(error));
-
-
-export const MENU = [
-    {
-        name: prueba.menu.name,
-        options: prueba.menu.options
-    }
-]
 
 export const PUBLIC_ROUTES = [
     '/'
 ];
-
 export const YES_NO_OPTIONS = [
     {
         id: "true",
